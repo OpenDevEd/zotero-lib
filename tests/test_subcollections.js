02f54e3ffa39ad57fa3824adc809c424869604a4
Björn Haßler <bjohas+git@gmail.com>
2021-01-17 17:01:22 +0000

    zotzen-workspace (tidy up)

--- tests/test_subcollections.js
+++ tests/test_subcollections.js
@@ -6,11 +6,11 @@ const fs = require('fs')
 
 async function main() {
     var zotero = new Zotero({ verbose: true, "group-id": 2259720 })
-    let report = await zotero.collections({
+    let collections = await zotero.collections({
         key: "HP6NALR4",
         terse: true
     })
-    console.log("final=" + JSON.stringify(report, null, 2))
+    console.log("final=" + JSON.stringify(collections, null, 2))
     return 0
 }
 
