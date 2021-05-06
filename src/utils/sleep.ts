7ab717cd96d9d93734e31802aa40767aa222c643
Arfan Liaqat <arfanliaqat10@gmail.com>
2021-05-06 17:05:21 +0500

    chore: fixup
    fix export w.r.t cjs

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,3 +1,3 @@
-export default function sleep(delayInMilliseconds: number) {
+export = function sleep(delayInMilliseconds: number) {
   return new Promise((resolve) => setTimeout(resolve, delayInMilliseconds));
-}
+};
