03e96fbb92599c968232b7c011791dfd15b03626
echaoub <abdochaoubi2000@gmail.com>
2024-03-26 17:16:43 +0000

    fixing test

--- tests/test_get_doi.js
+++ tests/test_get_doi.js
@@ -5,13 +5,11 @@ const Zotero = require('../build/zotero-lib');
 const fs = require('fs');
 
 async function main() {
-  var zotero = new Zotero({ verbose: false, 'group-id': 2259720 });
+  const zotero = new Zotero({ verbose: false, 'group-id': 2259720 });
   const key = 'NMF7H2HP';
   console.log('--------------------------------');
   const doi = await zotero.get_doi({ key: key });
   console.log(doi);
-  // console.log('TEMPORARY=' + JSON.stringify(item, null, 2));
-  // const item = await zotero.item({ key: key });
   return 0;
 }
 
